dee6b931c55914dec99c09bb0bdd841f4f6f5381
Alvaro Lopez Medina <alopezme@redhat.com>
2021-06-01 21:09:06 +0200

    Update config to force timeouts, Add an scheduler to automatically put entries in the cache

--- src/main/java/com/redhat/alopezme/retriesreproducer/RetriesreproducerApplication.java
+++ src/main/java/com/redhat/alopezme/retriesreproducer/RetriesreproducerApplication.java
@@ -1,13 +1,42 @@
 package com.redhat.alopezme.retriesreproducer;
 
+import com.redhat.alopezme.retriesreproducer.model.Book;
+import com.redhat.alopezme.retriesreproducer.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class RetriesreproducerApplication {
+
+	@Autowired
+	BookRepository bookRepository;
+
+	int iteration = 0;
+
+	Logger logger = LoggerFactory.getLogger(RetriesreproducerApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetriesreproducerApplication.class, args);
 	}
 
+	/**
+	 * This method introduces a new book with id [0,99] every 10 ms.
+	 * Every 10 seconds, it overrides all the books in the cache (1000)
+	 */
+	@Scheduled(fixedDelay = 10, initialDelay = 10_000)
+	public void scheduleFixedDelayTask() {
+		int bookId = iteration++ % 1_000;
+		logger.debug("Fixed delay task - " + System.currentTimeMillis() / 1000 + " Iteration " + bookId);
+
+		Book book = new Book(bookId,"Coding from home" ,"Álvaro López Medina",2021);
+		logger.info("PUT - " + book.toString());
+		bookRepository.insert(bookId, book);
+	}
 }
--- src/main/java/com/redhat/alopezme/retriesreproducer/controller/BookController.java
+++ src/main/java/com/redhat/alopezme/retriesreproducer/controller/BookController.java
@@ -37,6 +37,7 @@ public class BookController {
         return bookRepository.findById(id).toString();
     }
 
+
     /**
      * PUT
      */
